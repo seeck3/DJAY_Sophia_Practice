86a86817fea7138ed934bbe006c7d7bb8d3b8806
jaeho im <jaeim1117@gmail.com>
2020-04-18 20:24:02 -0700

    style css data / populate

--- boards/src/Components/Boards/JayLim/JayLimBoards.jsx
+++ boards/src/Components/Boards/JayLim/JayLimBoards.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import './styles.css';
 import PropTypes from "prop-types";
 
 import mock_data from "../../../MockData/Data";
 
 const JayLimBoards = props => {
-  console.log("this is fake data", mock_data);
+  console.log(mock_data[2]);
   return (
     <div>
-      <h1>This is Jay Boards</h1>
-      <p>게시판 여기다가 만드십시오</p>
+      <div>
+      <h1 className='header1'>This is Jay Boards</h1>
+      <h1 className='header2'>heading2</h1>
+      <h1 className='header3'>heading3</h1>
+      <h1 className='header4'>heading4</h1>
+      <p> jaes hompi</p>
       <p>
         <strong>Title</strong> : {mock_data[0].title}
         <br />
@@ -17,6 +22,13 @@ const JayLimBoards = props => {
         <strong>Author</strong> :{" "}
         {mock_data[0].author.firstName + " " + mock_data[0].author.lastName}
       </p>
+      </div>
+      <div>
+        <p className='title'>Title: {mock_data[0].title}</p>
+        <p>Content: {mock_data[0].content}</p>
+        <p>First Name: {mock_data[0].author.firstName}</p>
+        <p>Last Name: {mock_data[0].author.lastName}</p>
+      </div>
     </div>
   );
 };
