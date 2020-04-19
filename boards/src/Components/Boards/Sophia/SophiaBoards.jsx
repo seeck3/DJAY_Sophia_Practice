99e204858e76ead25f2f482ec06635eb5a1f9a2d
seeck3 <marco.dseo@gmail.com>
2020-04-18 23:18:33 -0400

    style css and data populate

--- boards/src/Components/Boards/Sophia/SophiaBoards.jsx
+++ boards/src/Components/Boards/Sophia/SophiaBoards.jsx
@@ -1,22 +1,22 @@
 import React from "react";
+import './styles.css'
 import PropTypes from "prop-types";
 
 import mock_data from "../../../MockData/Data";
 
 const SophiaBoards = props => {
-  console.log("this is fake data", mock_data);
+  console.log("this is fake data", mock_data[3]);
   return (
     <div>
-      <h1>This Is Sophia Boards</h1>
-      <p>Make your boards here</p>
-      <p>
-        <strong>Title</strong> : {mock_data[2].title}
-        <br />
-        <strong>Content</strong> : {mock_data[2].content}
-        <br />
-        <strong>Author</strong> :{" "}
-        {mock_data[2].author.firstName + " " + mock_data[2].author.lastName}
-      </p>
+     <div> <h1 className='header1'>This Is Sophia Boards</h1>
+    
+    </div>
+    <div>
+      <p>Title : {mock_data[0].title}</p>
+      <p>Content :{mock_data[0].content}</p>
+      <p>First Name :{mock_data[0].author.firstName}</p>
+      <p>Last Name :{mock_data[0].author.lastName}</p>
+    </div>
     </div>
   );
 };
